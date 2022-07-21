8b4cf22858563fb5dfe29c3631e6683f76fc0aa2
David SPORN <sporniket.studio@gmail.com>
2022-07-21 07:04:16 +0200

    draft kicad5 generator : internal api to generate kicad5 lib statements -- pins

--- src/electronic_symbol_generator_for_cad/kicad5/pins.py
+++ src/electronic_symbol_generator_for_cad/kicad5/pins.py
@@ -20,3 +20,157 @@ If not, see <https://www.gnu.org/licenses/>. 
 """
 
 from typing import List
+from enum import Enum
+
+from electronic_package_descriptor import PinDescription
+
+
+class SideOfComponent(Enum):
+    """
+    The 4 sides of a components will be designated like when looking at a map from above.
+    """
+
+    NORTH = "n"  # ^
+    EAST = "e"  # -->
+    SOUTH = "s"  # v
+    WEST = "w"  # <--
+
+
+# pin type to kicad electrical type
+elecTypeByValueOfTypeOfPin = {
+    "PWR": "W",
+    "GND": "W",
+    "DNC": "N",
+    "I": "I",
+    "ICLK": "I",
+    "O": "O",
+    "OCLK": "O",
+    "O3": "T",
+    "OCOL": "C",
+    "OEMT": "E",
+    "OPSV": "P",
+    "OPWR": "w",
+    "B3": "T",
+    "B": "B",
+}
+
+# pin type to kicad pin shape
+shapeTypeByValueOfTypeOfPin = {
+    "PWR": "",
+    "GND": "",
+    "DNC": "",
+    "I": "",
+    "ICLK": "C",
+    "O": "",
+    "OCLK": "C",
+    "O3": "",
+    "OCOL": "",
+    "OEMT": "",
+    "OPSV": "",
+    "OPWR": "",
+    "B3": "",
+    "B": "",
+}
+
+
+def toPinTowardsWest(
+    name: str, index: int, x: int, y: int, elecType: str, shapeType: str, unit: int = 0
+) -> List[str]:
+    """
+    Generate a pin that will touch the component at ``(x,y)``, and will point toward the left of the sheet.
+
+    The pin has a width of 300 mils
+    """
+    return [
+        f"X {name} {index} {x - 300} {y} 300 R 50 50 {unit} 0 {elecType} {shapeType}"
+    ]
+
+
+def toPinTowardsEast(
+    name: str, index: int, x: int, y: int, elecType: str, shapeType: str, unit: int = 0
+) -> List[str]:
+    """
+    Generate a pin that will touch the component at ``(x,y)``, and will point toward the right of the sheet.
+
+    The pin has a width of 300 mils
+    """
+    return [
+        f"X {name} {index} {x + 300} {y} 300 L 50 50 {unit} 0 {elecType} {shapeType}"
+    ]
+
+
+def toPinTowardsNorth(
+    name: str, index: int, x: int, y: int, elecType: str, shapeType: str, unit: int = 0
+) -> List[str]:
+    """
+    Generate a pin that will touch the component at ``(x,y)``, and will point toward the top of the sheet.
+
+    The pin has a width of 300 mils
+    """
+    return [
+        f"X {name} {index} {x} {y + 300} 300 D 50 50 {unit} 0 {elecType} {shapeType}"
+    ]
+
+
+def toPinTowardsSouth(
+    name: str, index: int, x: int, y: int, elecType: str, shapeType: str, unit: int = 0
+) -> List[str]:
+    """
+    Generate a pin that will touch the component at ``(x,y)``, and will point toward the bottom of the sheet.
+
+    The pin has a width of 300 mils
+    """
+    return [
+        f"X {name} {index} {x} {y - 300} 300 U 50 50 {unit} 0 {elecType} {shapeType}"
+    ]
+
+
+toPinBySideOfComponent = {
+    "n": toPinTowardsNorth,
+    "e": toPinTowardsEast,
+    "s": toPinTowardsSouth,
+    "w": toPinTowardsWest,
+}
+
+
+def toStackOfPins(
+    x: int,
+    y: int,
+    sideOfComponent: SideOfComponent,
+    offset: int,
+    pins: List[PinDescription],
+    unit: int = 0,
+) -> List[str]:
+    locx = x
+    locy = y
+    dx = (
+        offset
+        if sideOfComponent == SideOfComponent.NORTH
+        or sideOfComponent == SideOfComponent.SOUTH
+        else 0
+    )
+    dy = (
+        offset
+        if sideOfComponent == SideOfComponent.EAST
+        or sideOfComponent == SideOfComponent.WEST
+        else 0
+    )
+    toPin = toPinBySideOfComponent[sideOfComponent.value]
+    result = []
+    for pin in pins:
+        if pin != None:
+            typeOfPin = pin.type.value
+            result.extend(
+                toPin(
+                    pin.name,
+                    pin.designator.fullname,
+                    locx,
+                    locy,
+                    elecTypeByValueOfTypeOfPin[typeOfPin],
+                    shapeTypeByValueOfTypeOfPin[typeOfPin],
+                    unit,
+                )
+            )
+        locx += dx
+        locy += dy
+    return result
